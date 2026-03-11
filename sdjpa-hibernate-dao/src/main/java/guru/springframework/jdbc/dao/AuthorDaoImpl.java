1eed98a7035aae1b6ee7e26d7d26f8907a9df38f
tommic81 <tom.michnik@gmail.com>
2026-03-11 07:36:52 +0100

    Implemented Update an Author

--- sdjpa-hibernate-dao/src/main/java/guru/springframework/jdbc/dao/AuthorDaoImpl.java
+++ sdjpa-hibernate-dao/src/main/java/guru/springframework/jdbc/dao/AuthorDaoImpl.java
@@ -47,7 +47,12 @@ public class AuthorDaoImpl implements AuthorDao {
 
     @Override
     public Author updateAuthor(Author author) {
-        return null;
+        EntityManager em = getEntityManager();
+        em.joinTransaction();
+        em.merge(author);
+        em.flush();
+        em.clear();
+        return em.find(Author.class, author.getId());
     }
 
     @Override
